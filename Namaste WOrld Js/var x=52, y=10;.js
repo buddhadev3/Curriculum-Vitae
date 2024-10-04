99f2836e1497c2aa32e10a520e382ba04d6f5d0b
buddhadev3 <buddhadev20022018@gmail.com>
2024-10-04 16:56:51 +0530

    Add files via upload

---
+++ Namaste WOrld Js/var x=52, y=10;.js
@@ -0,0 +1,74 @@
+var x=52, y=10;
+x=x%y;
+x
+
+var a=10 , b=2
+a=a**2
+a
+
+var x = 3 == 3;
+x
+
+var x = 3 == "3";
+x
+
+var x = 3 === "3";
+x
+
+var x = 3 != "3";
+x
+
+var x = 3 != 3;
+x
+
+var x = 3 != 4;
+x
+
+var x = 3 >= 4;
+x
+
+var x = 3 > 3;
+x
+
+var x = 3 >= 3;
+x
+
+var x = 7 < 4;
+x
+
+var x = 3 <= 3;
+x
+
+var x = 7;
+x++;
+x
+
+var x = 7;
+x--;
+x
+
+var x = 7;
+x=-x;
+x
+
+var x = 7;
+x=+x;
+x
+
+var x = true;
+x=+x;
+x
+
+var x = false;
+x=-x;
+x
+
+var x ="Nanaste", y="world";
+x=x+y;
+x
+
+var x ="Nanaste", y="world";
+x=x +" "+ y;
+x
+
+
